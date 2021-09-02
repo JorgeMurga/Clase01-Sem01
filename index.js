ecb9c3507cb8fb0b581f162bc443fa05d91daa95
Jorge <SV73227718@idat.edu.pe>
2021-09-02 18:08:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hola');
+
+console.log(2+7)
+
+let date = new Date();
+
+console.log('Hoy es ' + date);
+console.log(`Today's day is ${date}`)
+
+document.body.innerHTML = `<h1>Today's date is ${date}</h1>`;
+document.body.style.backgroundColor = 'lightblue';
