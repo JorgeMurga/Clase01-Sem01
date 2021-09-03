ab6a23362d4085a117116395e6e6d8a25defc0a9
Jorge <SV73227718@idat.edu.pe>
2021-09-03 16:20:11 -0500

    "master"

--- index.js
+++ index.js
@@ -3,15 +3,49 @@
 // let admin = name;
 
 2
-
+let Tierra = ''; 
+let Variable = '';
 
 3
-// const birthday = '18.04.1982';
-// const age = someCode(birthday);
+const birthday = '18.04.1982';
+const age = someCode(birthday);
 
 4
 // let name = "Ilya";
 // alert(`hello ${name}`);
+
+5
+// let user = {}
+// name: "John",
+// surname: "Smith";
+// user.name = "Pete"
+// delete
+
+6
+
+
+7
+
+8
+// before the call
+let menu = {
+ width: 200,
+ height: 300,
+ title: "My menu"
+};
+
+function multiplyNumeric(object) {
+    for (key in object) {
+        if (typeof object[key] === "number") {
+            object[key] = object[key] * 2;
+          }
+        }
+      }
+      multiplyNumeric(menu);
+      console.log(menu);
+
+
+
 
 
 
